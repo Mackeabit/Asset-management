6aaf1c23d1bd8a25e2343e6527986a4dde980ffe
Mackeabit <thdrudgh7775@naver.com>
2022-05-14 20:56:50 +0900

    Create UserVO.class

--- Asset_management/src/vo/UserVO.java
+++ Asset_management/src/vo/UserVO.java
@@ -1,5 +1,38 @@
 package vo;
 
 public class UserVO {
-
+	private int idx;
+	private String name, id, pwd, accountNumber;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
 }
