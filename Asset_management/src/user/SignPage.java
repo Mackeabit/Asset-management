0992fe77df0980ce2e9f34fec8b5de19926f5ae5
Mackeabit <thdrudgh7775@naver.com>
2022-05-17 22:41:54 +0900

    Add check User code

--- Asset_management/src/user/SignPage.java
+++ Asset_management/src/user/SignPage.java
@@ -16,10 +16,10 @@ import javax.swing.JOptionPane;
 public class SignPage {
 
 	public SignPage() {
-		
-		//프레임 생성
+
+		// 프레임 생성
 		Frame mainFrame = new Frame("회원가입");
-		//라벨 생성
+		// 라벨 생성
 		Label mainLabel = new Label("Sign Up");
 		Label account = new Label("계좌번호 :");
 		Label acml = new Label("숫자만 입력 가능합니다.");
@@ -27,16 +27,16 @@ public class SignPage {
 		Label idml = new Label("영문,숫자만 입력 가능합니다.", Label.RIGHT);
 		Label pwl = new Label("비밀번호 :", Label.RIGHT);
 		Label pwcheck = new Label("PW 확인 : ", Label.RIGHT);
-		//사용할 폰트 설정
+		// 사용할 폰트 설정
 		Font mainFont = new Font("", Font.BOLD, 40);
 		Font subFont = new Font("", Font.BOLD, 17);
 		Font smlFont = new Font("", Font.PLAIN, 11);
-		//텍스트필드 생성
+		// 텍스트필드 생성
 		TextField actf = new TextField(20);
 		TextField idtf = new TextField(20);
 		TextField pwtf = new TextField(15);
 		TextField pwchecktf = new TextField(15);
-		//버튼 성생
+		// 버튼 성생
 		Button checkAc = new Button("계좌조회");
 		Button checkAc2 = new Button("계좌수정");
 		Button checkID = new Button("중복체크");
@@ -47,26 +47,25 @@ public class SignPage {
 		boolean check1 = false;
 		boolean check2 = false;
 
-		//메인 프레임 설정
+		// 메인 프레임 설정
 		mainFrame.setResizable(false);
 		mainFrame.setLayout(null);
 		mainFrame.setVisible(true);
 		mainFrame.setBackground(Color.WHITE);
-		
-		//비밀번호 별표처리
+
+		// 비밀번호 별표처리
 		pwtf.setEchoChar('*');
 		pwchecktf.setEchoChar('*');
 
-		//X키 활성화
+		// X키 활성화
 		mainFrame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
-		
-		
-		//프레임, 라벨, 텍스트필드, 버튼 배치
+
+		// 프레임, 라벨, 텍스트필드, 버튼 배치
 		mainFrame.setBounds(480, 200, 450, 550);
 		mainLabel.setBounds(145, 100, 200, 90);
 		account.setBounds(57, 202, 90, 50);
@@ -85,8 +84,8 @@ public class SignPage {
 		checkID2.setBounds(310, 269, 70, 25);
 		sign.setBounds(155, 445, 140, 45);
 		goMain.setBounds(19, 42, 60, 25);
-		
-		//폰트 및 색상 설정
+
+		// 폰트 및 색상 설정
 		mainLabel.setFont(mainFont);
 		idl.setFont(subFont);
 		idml.setFont(smlFont);
@@ -103,8 +102,8 @@ public class SignPage {
 		pwl.setForeground(Color.DARK_GRAY);
 		pwcheck.setForeground(Color.DARK_GRAY);
 		account.setForeground(Color.DARK_GRAY);
-		
-		//프레임에 등록
+
+		// 프레임에 등록
 		mainFrame.add(mainLabel);
 		mainFrame.add(idl);
 		mainFrame.add(pwl);
@@ -120,38 +119,45 @@ public class SignPage {
 		mainFrame.add(checkID2);
 		mainFrame.add(actf);
 		mainFrame.add(goMain);
-		
-		//중복체크 버튼 설정(true값 입력시 수정활성화)
+
+		// 중복체크 버튼 설정(true값 입력시 수정활성화)
 		checkAc2.setVisible(false);
 		checkID2.setVisible(false);
-		
-		//버튼 동작 설정
+
+		// 버튼 동작 설정
 		ActionListener al = new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				String account = actf.getText();
-				String id = idtf.getText();
-				String pw = pwtf.getText();
-				String pw2 = pwchecktf.getText();
+				String account = actf.getText(); // 계좌
+				String id = idtf.getText(); // 아이디
+				String pwd = pwtf.getText(); // 비밀번호
+				String pwd2 = pwchecktf.getText(); // 비밀번호 확인
 
-				if (!account.equals(actf.getText().trim())) {
+				if (!account.equals(actf.getText().replaceAll(" ", ""))) {
 					JOptionPane.showMessageDialog(mainFrame, "계좌번호에 공백이 포함되면 안됩니다.");
-				} else if (!id.equals(idtf.getText().trim())) {
+				} else if (!id.equals(idtf.getText().replaceAll(" ", ""))) {
 					JOptionPane.showMessageDialog(mainFrame, "아이디에 공백이 포함되면 안됩니다.");
-				} else if (!pw.equals(pwtf.getText().trim())) {
+				} else if (!pwd.equals(pwtf.getText().replaceAll(" ", ""))) {
 					JOptionPane.showMessageDialog(mainFrame, "비밀번호에 공백이 포함되면 안됩니다.");
 				} else {
 
 					switch (e.getActionCommand()) {
 
 					case "계좌조회":
-
+						
 						break;
 
 					case "중복체크":
 
+						break;
+
+					case "계좌수정":
+
+						break;
+
+					case "ID수정":
 
 						break;
 
@@ -165,20 +171,12 @@ public class SignPage {
 						new LoginPage();
 						break;
 
-					case "계좌수정":
-
-						break;
-
-					case "ID수정":
-
-						break;
-
 					}
 				}
 			}
 		};
-		
-		//버튼 액션 등록
+
+		// 버튼 액션 등록
 		checkAc.addActionListener(al);
 		checkAc2.addActionListener(al);
 		checkID.addActionListener(al);
