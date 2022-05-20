d5443f198366e050bb4a16c2aeed31511d8a1306
Mackeabit <thdrudgh7775@naver.com>
2022-05-20 18:57:32 +0900

    메인 실행 시 관리자, 유저 폴더 생성 코드추가

--- Asset_management/src/user/AssetMain.java
+++ Asset_management/src/user/AssetMain.java
@@ -1,10 +1,16 @@
 package user;
 
+import dao.UserDAO;
+
 public class AssetMain {
 	public static void main(String[] args) {
 		
 		//로그인 페이지 객체 생성
 		new LoginPage();
+		
+		//실행 시 DAO를 통해 유저, 관리자 폴더 생성
+		UserDAO.getInstance().adminWrite();
+		
 		
 	}
 }
